af33e7480e6422f5bb7b838a74924867491e15c7
Ziv Perry <ziv.perry@gmail.com>
2025-08-25 20:57:05 +0300

    builder test

--- web/test/builder_test.ts
+++ web/test/builder_test.ts
@@ -1,7 +1,11 @@
+import { expect } from "@std/expect";
 import { build } from "../builder.ts";
 import { Feat1 } from "./testing-blocks.ts";
 
 Deno.test("builder_test", () => {
   const data = build(Feat1);
-  console.log(data);
+  expect(data).toHaveProperty("ctr", Feat1);
+  expect(data).toHaveProperty("controllers");
+  // todo complete tests
+  // console.log(data);
 });
