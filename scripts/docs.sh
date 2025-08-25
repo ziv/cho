6cddfa58c9da13a3d6a89b031c5aa8c5625cdb1d
Ziv Perry <ziv.perry@gmail.com>
2025-08-25 15:59:12 +0300

    replace mac command with linux one

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -6,4 +6,9 @@ deno doc --html  core/di/mod.ts core/utils/mod.ts web/mod.ts
 # fix the generated docs (replace 〇 with @)
 # shellcheck disable=SC2164
 cd ./docs
-find . -type f -name "*.*" -exec sed -i '' 's/〇/@/g' {} +
+
+# this is mac version
+#find . -type f -name "*.html" -exec sed -i '' 's/〇/@/g' {} +
+
+# this is linux version
+find . -type f -name "*.html" -exec sed -i 's/〇/@/g' {} +
