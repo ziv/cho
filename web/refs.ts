6961bdfc55c3767e55fa6ee2ed3e96a9f380feef
Ziv Perry <ziv.perry@gmail.com>
2025-08-25 22:02:08 +0300

    fix types

--- web/refs.ts
+++ web/refs.ts
@@ -1,41 +1,46 @@
-import {Injector, type Target} from "@chojs/core";
+import type { Injector, Instance, Target } from "@chojs/core";
+import type {
+  ControllerDescriptor,
+  FeatureDescriptor,
+  MethodDescriptor,
+} from "./types.ts";
 
 /**
  * A reference to a method, including its descriptor, handler function, and middlewares.
  */
 export class MethodRef {
-    constructor(
-        readonly desc: MethodDescriptor,
-        readonly handler: Target,
-        readonly middlewares: Target[],
-    ) {
-    }
+  constructor(
+    readonly desc: MethodDescriptor,
+    readonly handler: Target,
+    readonly middlewares: Target[],
+  ) {
+  }
 }
 
 /**
  * A reference to a controller, including its descriptor, instance, middlewares, and methods.
  */
 export class ControllerRef {
-    constructor(
-        readonly desc: ControllerDescriptor,
-        readonly instance: Instance,
-        readonly middlewares: Target[],
-        readonly methods: MethodRef[],
-    ) {
-    }
+  constructor(
+    readonly desc: ControllerDescriptor,
+    readonly instance: Instance,
+    readonly middlewares: Target[],
+    readonly methods: MethodRef[],
+  ) {
+  }
 }
 
 /**
  * A reference to a feature, including its descriptor, instance, middlewares, injector, sub-features, and controllers.
  */
 export class FeatureRef {
-    constructor(
-        readonly desc: FeatureDescriptor,
-        readonly instance: Instance,
-        readonly middlewares: Target[],
-        readonly injector: Injector,
-        readonly features: FeatureRef[],
-        readonly controllers: ControllerRef[],
-    ) {
-    }
+  constructor(
+    readonly desc: FeatureDescriptor,
+    readonly instance: Instance,
+    readonly middlewares: Target[],
+    readonly injector: Injector,
+    readonly features: FeatureRef[],
+    readonly controllers: ControllerRef[],
+  ) {
+  }
 }
