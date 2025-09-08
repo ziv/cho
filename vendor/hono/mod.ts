48011139cde020a20ebe84ff842d623e9c9ef33c
Ziv Perry <ziv.perry@zoominfo.com>
2025-09-08 17:15:44 +0300

    fix: hono-adapter

--- vendor/hono/mod.ts
+++ vendor/hono/mod.ts
@@ -1,2 +1,9 @@
+/**
+ * # Hono Adapter for CHO Web
+ *
+ * Integrate Hono with CHO web framework.
+ *
+ * @see https://ziv.github.io/cho/
+ */
 export * from "./hono-adapter.ts";
 export * from "./hono-context.ts";
