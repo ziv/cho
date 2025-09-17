36aec667944e16dfd0bf6326ec0a9ea4b3801385
Ziv Perry <ziv.perry@zoominfo.com>
2025-09-17 12:17:31 +0300

    feat(core): public release
    BREAKING CHANGE: product descion

--- core/.releaserc.mjs
+++ core/.releaserc.mjs
@@ -1,15 +1,15 @@
 export default {
-  tagFormat: "core-v${version}",
-  plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    "@semantic-release/github",
-    [
-      "@semantic-release/exec",
-      {
-        prepareCmd: 'sed -i "s/0.0.0/${nextRelease.version}/g" deno.json && cat deno.json',
-        publishCmd: "npx jsr publish --allow-dirty",
-      },
+    tagFormat: "core-v${version}",
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        "@semantic-release/github",
+        [
+            "@semantic-release/exec",
+            {
+                prepareCmd: 'sed -i "s/0.0.0/${nextRelease.version}/g" deno.publish.json && rm deno.json && mv deno.publish.json deno.json &&  cat deno.json',
+                publishCmd: "npx jsr publish --allow-dirty",
+            },
+        ],
     ],
-  ],
 };
